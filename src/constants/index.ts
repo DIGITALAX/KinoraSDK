bfcad493b453c1a48d853245b0fc32452692fd03
emmajane1313 <admin@digitalax.xyz>
2023-08-30 21:33:34 -0400

    user auth

---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+export const KINORA_PKP_DB: `0x${string}` = "0x";
+
+export const PKP_CONTRACT_ADDRESS: `0x${string}` =
+  "0x8F75a53F65e31DD0D2e40d0827becAaE2299D111";
+
+export const LIT_RPC: string = "https://chain-rpc.litprotocol.com/http";
+
+export const IPFS_CID_PKP: string = "";
--- src/sequence.ts
+++ src/sequence.ts
@@ -1,18 +1,373 @@
-import { LivepeerPlayer } from "./@types/kinora-sdk";
+import { ChainIds, LivepeerPlayer } from "./@types/kinora-sdk";
 import { Metrics } from "./metrics";
+import "@lit-protocol/lit-auth-client";
+import * as LitJsSdk_authHelpers from "@lit-protocol/auth-helpers";
+import * as LitJsSdk from "@lit-protocol/lit-node-client";
+import { ProviderType } from "@lit-protocol/constants";
+import { PKPEthersWallet } from "@lit-protocol/pkp-ethers";
+import { SiweMessage } from "siwe";
+import { ethers } from "ethers";
+import { AuthMethod, SessionSigs, IRelayPKP } from "@lit-protocol/types";
+import { KINORA_PKP_DB, LIT_RPC, PKP_CONTRACT_ADDRESS } from "./constants";
+import KinoraPKPDB from "./abis/KinoraPKPDB.json";
+import PKPNFT from "./abis/PKPNFT.json";
+import { BaseProvider } from "@lit-protocol/lit-auth-client";
+import { createTxData, litExecute } from "./utils/lit-protocol";
+import { JsonRpcProvider } from "@ethersproject/providers";
 
 export class Sequence<TPlaybackPolicyObject extends object, TSlice> {
   private livepeerPlayer: LivepeerPlayer<TPlaybackPolicyObject, TSlice>;
   private metrics: Metrics;
   private startTime: number = 0;
+  private litAuthClient = new LitAuthClient({
+    litRelayConfig: {
+      relayApiKey: `${process.env.LIT_RELAY_KEY}`,
+    },
+  });
+  private litNodeClient = new LitNodeClient({
+    litNetwork: "serrano",
+    debug: false,
+    alertWhenUnauthorized: true,
+  });
+  private currentPKP: {
+    ethAddress: string;
+    publicKey: string;
+    tokenId: string;
+    sessionSig: SessionSigs;
+    pkpWallet: PKPEthersWallet;
+    authSig: {
+      sig: any;
+      derivedVia: string;
+      signedMessage: string;
+      address: any;
+    };
+    encryptedToken: string;
+  };
+  private redirectURL: string;
+  private providerType: ProviderType;
+  private litProvider: BaseProvider;
+  private questProvider: JsonRpcProvider;
+  private chronicleProvider: JsonRpcProvider;
+  private authMethod: AuthMethod;
+  private rpcURL: string;
+  private chain: string = "polygon";
 
-  constructor(livepeerPlayer: LivepeerPlayer<TPlaybackPolicyObject, TSlice>) {
+  constructor(
+    livepeerPlayer: LivepeerPlayer<TPlaybackPolicyObject, TSlice>,
+    redirectURL: string,
+    rpcURL: string
+  ) {
     this.livepeerPlayer = livepeerPlayer;
     this.metrics = new Metrics();
     this.bindEvents();
+    this.redirectURL = redirectURL;
+    this.rpcURL = rpcURL;
+    this.questProvider = new ethers.providers.JsonRpcProvider(
+      this.rpcURL,
+      ChainIds[this.chain]
+    );
+    this.chronicleProvider = new ethers.providers.JsonRpcProvider(
+      LIT_RPC,
+      175177
+    );
   }
 
-  bindEvents() {
+  authenticateUser = async (type: "wallet" | "google" | "discord") => {
+    try {
+      this.providerType =
+        type === "wallet"
+          ? ProviderType.EthWallet
+          : type === "discord"
+          ? ProviderType.Discord
+          : ProviderType.Google;
+
+      this.litProvider = this.litAuthClient.initProvider(this.providerType, {
+        redirectUri: `${this.redirectURL}`,
+      });
+      await this.litProvider.signIn();
+    } catch (err: any) {
+      // add in error logs aqui
+    }
+  };
+
+  authenticateRedirect = async () => {
+    try {
+      this.authMethod = await this.litProvider.authenticate();
+
+      const values = await this.handlePKPs();
+
+      this.currentPKP = {
+        ...values?.currentPKP,
+        sessionSig: values?.sessionSigs,
+        pkpWallet: values?.pkpWallet,
+        authSig: values?.authSig,
+        encryptedToken: values?.encryptedToken,
+      };
+    } catch (err: any) {
+      // add in error logs aqui
+    }
+  };
+
+  private handlePKPs = async () => {
+    try {
+      let res = await this.fetchPkp();
+      if (res == undefined) {
+        res = await this.mintPkp();
+      }
+      const sessionSigs = await this.getSessionSig(res);
+      const pkpWallet = new PKPEthersWallet({
+        controllerSessionSigs: sessionSigs,
+        pkpPubKey: res.publicKey,
+        rpc: this.rpcURL,
+      });
+      const authSig = await this.generateAuthSignature(pkpWallet);
+
+      const kinoraPkpDBContract = new ethers.Contract(
+        KINORA_PKP_DB,
+        KinoraPKPDB,
+        this.questProvider
+      );
+
+      const doesExist = await kinoraPkpDBContract.userExists(
+        BigInt(res?.tokenId).toString()
+      );
+
+      if (!doesExist) {
+        await this.storeAuthOnChain(authSig, res);
+      }
+
+      const encryptedToken = await this.encryptToken(
+        res.ethAddress as `0x${string}`,
+        authSig,
+        BigInt(res.tokenId).toString()
+      );
+
+      return {
+        currentPKP: res,
+        encryptedToken,
+        authSig,
+        pkpWallet,
+        sessionSigs,
+      };
+    } catch (err: any) {
+      console.error(err.message);
+    }
+  };
+
+  private getSessionSig = async (currentPKP): Promise<SessionSigs> => {
+    try {
+      await this.litNodeClient.connect();
+
+      const litResource = new LitJsSdk_authHelpers.LitPKPResource(
+        currentPKP.tokenId.hex
+      );
+
+      const sessionSigs = await this.litProvider.getSessionSigs({
+        pkpPublicKey: this.currentPKP.publicKey,
+        authMethod: {
+          authMethodType:
+            this.providerType === ProviderType.Google
+              ? 6
+              : this.providerType === ProviderType.Discord
+              ? 4
+              : 1,
+          accessToken: this.authMethod.accessToken,
+        },
+        sessionSigsParams: {
+          chain: this.chain,
+          resourceAbilityRequests: [
+            {
+              resource: litResource,
+              ability: LitJsSdk_authHelpers.LitAbility.PKPSigning,
+            },
+          ],
+        },
+        litNodeClient: this.litNodeClient,
+      });
+      return sessionSigs;
+    } catch (e: any) {
+      console.error(e.message);
+    }
+  };
+
+  private storeAuthOnChain = async (
+    authSig: {
+      sig: any;
+      derivedVia: string;
+      signedMessage: string;
+      address: any;
+    },
+    currentPKP: IRelayPKP
+  ) => {
+    try {
+      const tx = await createTxData(
+        this.questProvider,
+        KinoraPKPDB,
+        KINORA_PKP_DB,
+        "createUserPKPAccount",
+        [BigInt(currentPKP?.tokenId!).toString()],
+        ChainIds[this.chain]
+      );
+
+      await litExecute(
+        this.questProvider,
+        this.litNodeClient,
+        tx,
+        "createUserPKPAccount",
+        authSig,
+        currentPKP.publicKey as `0x04${string}`
+      );
+    } catch (err: any) {
+      console.error(err.message);
+    }
+  };
+
+  private mintPkp = async (): Promise<IRelayPKP> => {
+    // mint a new pkp
+    if (this.litProvider && this.authMethod) {
+      const pkpContract = new ethers.Contract(PKP_CONTRACT_ADDRESS, PKPNFT);
+      const txHash = await this.litProvider.mintPKPThroughRelayer(
+        this.authMethod
+      );
+      const receipt = await this.chronicleProvider.getTransactionReceipt(
+        txHash
+      );
+
+      if (receipt && receipt.logs) {
+        const parsedLogs = receipt.logs
+          .map((log) => {
+            try {
+              return pkpContract.interface.parseLog(log);
+            } catch (e) {
+              return null;
+            }
+          })
+          .filter((parsedLog) => parsedLog !== null);
+
+        const filteredLogs = parsedLogs.filter((parsedLog) => {
+          return parsedLog.name === "Transfer";
+        });
+
+        const tokenId = filteredLogs[0].args[2];
+        const publicKey = await pkpContract.readContract({
+          address: PKP_CONTRACT_ADDRESS,
+          abi: PKPNFT,
+          functionName: "getPubkey",
+          args: [tokenId],
+        });
+        return {
+          ethAddress: ethers.utils.computeAddress(publicKey as any),
+          publicKey: publicKey,
+          tokenId: tokenId.toHexString(),
+        };
+      }
+    }
+  };
+
+  private fetchPkp = async () => {
+    try {
+      if (this.litProvider && this.authMethod) {
+        const res = await this.litProvider.fetchPKPsThroughRelayer(
+          this.authMethod
+        );
+        // get all the pkps stored on chain
+        const { data } = await getPKPs();
+        let result = res[0];
+        if (data?.orderCreateds?.length > 0 && res?.length > 0) {
+          for (let i = 0; i < data?.orderCreateds?.length; i++) {
+            for (let j = 0; j < res?.length; j++) {
+              if (
+                data?.orderCreateds[i]?.toLowerCase() ===
+                BigInt(res[j]?.tokenId!).toString()
+              ) {
+                result = res[j];
+                return;
+              }
+            }
+          }
+        }
+        return result;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  private encryptToken = async (
+    address: `0x${string}`,
+    authSig: {
+      sig: any;
+      derivedVia: string;
+      signedMessage: string;
+      address: any;
+    },
+    currentPKP: string
+  ): Promise<string | undefined> => {
+    try {
+      let encryptedTokenId: string | undefined;
+
+      const { encryptedString, symmetricKey } = await LitJsSdk.encryptString(
+        currentPKP
+      );
+
+      const encryptedSymmetricKey = await this.litNodeClient.saveEncryptionKey({
+        accessControlConditions: [
+          {
+            contractAddress: "",
+            standardContractType: "",
+            chain: this.chain,
+            method: "",
+            parameters: [":userAddress"],
+            returnValueTest: {
+              comparator: "=",
+              value: address?.toLowerCase() as string,
+            },
+          },
+        ],
+        symmetricKey,
+        authSig,
+        chain: this.chain,
+      });
+
+      const buffer = await encryptedString.arrayBuffer();
+      encryptedTokenId = JSON.stringify({
+        encryptedString: JSON.stringify(Array.from(new Uint8Array(buffer))),
+        encryptedSymmetricKey: LitJsSdk.uint8arrayToString(
+          encryptedSymmetricKey,
+          "base16"
+        ),
+      });
+
+      return encryptedTokenId;
+    } catch (err: any) {
+      console.error(err.message);
+    }
+  };
+
+  private generateAuthSignature = async (pkpWallet: any | undefined) => {
+    try {
+      const siweMessage = new SiweMessage({
+        domain: "coinop.themanufactory.xyz",
+        address: pkpWallet?.address,
+        statement: "This is an Auth Sig for Coin Op",
+        uri: "https://coinop.themanufactory.xyz",
+        version: "1",
+        chainId: 137,
+      });
+      const signedMessage = siweMessage.prepareMessage();
+      const sig = await pkpWallet?.signMessage(signedMessage);
+      return {
+        sig,
+        derivedVia: "web3.eth.personal.sign",
+        signedMessage,
+        address: pkpWallet?.address,
+      };
+    } catch (err: any) {
+      console.error(err.message);
+    }
+  };
+
+  bindEvents = () => {
     this.livepeerPlayer.on("stream.started", () => {
       this.startTime = Date.now();
       this.metrics.updateCTR();
@@ -30,11 +385,9 @@ export class Sequence<TPlaybackPolicyObject extends object, TSlice> {
       }
       this.metrics.updateBounce();
     });
+  };
 
-    
-  }
-
-  getLogs() {
+  getLogs = () => {
     const logs = {
       AverageViewDuration: this.metrics.getAVD(),
       ClickThroughRate: this.metrics.getCTR(),
@@ -42,7 +395,7 @@ export class Sequence<TPlaybackPolicyObject extends object, TSlice> {
       // ReturnVisitorRate: this.metrics.getReturnVisitorRate(),
     };
     return logs;
-  }
+  };
 }
 
 // Engagement
