aaafcdfcdbcfcc0251753d74156c7efbc113612f
emmajane1313 <admin@digitalax.xyz>
2023-10-27 11:29:33 -0400

    add all comments

--- src/utils/lit-protocol.ts
+++ src/utils/lit-protocol.ts
@@ -3,11 +3,20 @@ import { CHRONICLE_PKP_CONTRACT, DENO_BUNDLED } from "./../../src/constants";
 import { joinSignature } from "@ethersproject/bytes";
 import { serialize } from "@ethersproject/transactions";
 import bs58 from "bs58";
-import * as LitJsSdk_authHelpers from "@lit-protocol/auth-helpers";
 import * as LitJsSdk from "@lit-protocol/lit-node-client";
 import { SiweMessage } from "siwe";
+import {
+  ContractABI,
+  GeneratedTxData,
+  LitAuthSig,
+} from "./../../src/@types/kinora-sdk";
 let crypto: any, CryptoJS: any;
 
+/**
+ * @function loadNodebuild
+ * @description Dynamically imports crypto and crypto-js libraries if running in a Node.js environment.
+ * @returns {Promise<void>}
+ */
 const loadNodebuild = async () => {
   if (typeof window === "undefined") {
     crypto = await import("crypto");
@@ -15,17 +24,19 @@ const loadNodebuild = async () => {
   }
 };
 
+// Invokes the function to ensure necessary libraries are loaded in Node.js environment.
 loadNodebuild();
 
-import {
-  ChainIds,
-  ContractABI,
-  GeneratedTxData,
-  LitAuthSig,
-  PlayerMetrics,
-} from "./../../src/@types/kinora-sdk";
-import { IRelayPKP, SessionSigs } from "@lit-protocol/types";
-
+/**
+ * @function createTxData
+ * @description Asynchronously generates transaction data necessary for interacting with a smart contract.
+ * @param {ethers.providers.JsonRpcProvider} provider - The Ethereum provider instance.
+ * @param {ContractABI} abi - The ABI of the contract you are interacting with.
+ * @param {string} functionName - The name of the function in the smart contract that you want to call.
+ * @param {any[]} args - An array of arguments to pass to the smart contract function.
+ * @returns {Promise<{error?: boolean, message?: string, generatedTxData?: GeneratedTxData}>} -
+ *   A promise that resolves to an object containing either an error or the generated transaction data.
+ */
 export const createTxData = async (
   provider: ethers.providers.JsonRpcProvider,
   abi: ContractABI,
@@ -66,6 +77,23 @@ export const createTxData = async (
   }
 };
 
+/**
+ * @function litExecute
+ * @description Asynchronously executes a transaction via Lit (Lightning Identity Token) protocol,
+ *   handling retries in case of timeout or underpriced errors.
+ * @param {ethers.providers.JsonRpcProvider} provider - The Ethereum provider instance.
+ * @param {any} litClient - The Lit client instance.
+ * @param {GeneratedTxData} tx - The transaction data.
+ * @param {string} sigName - The signature name.
+ * @param {LitAuthSig} authSig - The Lit authentication signature.
+ * @param {string} ipfsHash - The IPFS hash of the data being transacted.
+ * @param {string} publicKey - The public key of the transactor.
+ * @param {string} multihashDevKey - The developer's multihash key.
+ * @param {string} hashKeyItem - The hash key item (to combine with the multikeyhash).
+ * @param {number} retryCount - The current retry count (defaults to 0).
+ * @returns {Promise<{error: boolean, txHash?: string, message?: string, litResponse?: any}>} -
+ *   A promise that resolves to an object containing either an error or the transaction hash and Lit response.
+ */
 export const litExecute = async (
   provider: ethers.providers.JsonRpcProvider,
   litClient: any,
@@ -161,6 +189,16 @@ export const litExecute = async (
   }
 };
 
+/**
+ * @function generateAuthSig
+ * @description Asynchronously generates an authentication signature for KinoraSDK.
+ * @param {ethers.Signer} signer - The signer instance for signing the message.
+ * @param {number} [chainId=1] - The chain ID, defaults to 1.
+ * @param {string} [uri="https://localhost/login"] - The URI, defaults to "https://localhost/login".
+ * @param {string} [version="1"] - The version, defaults to "1".
+ * @returns {Promise<{error?: boolean, message?: string, litAuthSig?: LitAuthSig}>} -
+ *   A promise that resolves to an object containing either an error or the Lit authentication signature.
+ */
 export const generateAuthSig = async (
   signer: ethers.Signer,
   chainId = 1,
@@ -196,11 +234,23 @@ export const generateAuthSig = async (
   }
 };
 
+/**
+ * @function getBytesFromMultihash
+ * @description Converts a multihash string to a hexadecimal string.
+ * @param {string} multihash - The multihash string.
+ * @returns {string} - The hexadecimal representation of the multihash.
+ */
 export const getBytesFromMultihash = (multihash: string): string => {
   const decoded = bs58.decode(multihash);
   return `0x${Buffer.from(decoded).toString("hex")}`;
 };
 
+/**
+ * @function hashHex
+ * @description Hashes a hexadecimal string using SHA-256.
+ * @param {string} input - The input hexadecimal string.
+ * @returns {string} - The SHA-256 hash of the input string.
+ */
 export const hashHex = (input: string): string => {
   if (!CryptoJS) {
     throw new Error("This function can only be run in a Node.js environment.");
@@ -209,6 +259,17 @@ export const hashHex = (input: string): string => {
   return "0x" + hash.toString(CryptoJS.enc.Hex);
 };
 
+/**
+ * @function encryptMetrics
+ * @description Asynchronously encrypts metrics data based on specified conditions and authorization signature.
+ * @param {string} metrics - The metrics data as a string.
+ * @param {string} questInvokerPKPAddress - The quest invoker's PKP address.
+ * @param {string} playerPKPAddress - The player's PKP address.
+ * @param {LitAuthSig} playerPKPAuthSig - The player's PKP authorization signature.
+ * @param {LitJsSdk.LitNodeClient} litNodeClient - The Lit Node Client instance.
+ * @returns {Promise<{encryptedString?: string, error?: boolean, message?: string}>} -
+ *   A promise that resolves to an object containing either an error or the encrypted metrics string.
+ */
 export const encryptMetrics = async (
   metrics: string,
   questInvokerPKPAddress: `0x${string}`,
@@ -268,6 +329,17 @@ export const encryptMetrics = async (
   }
 };
 
+/**
+ * @function decryptMetrics
+ * @description Asynchronously decrypts encrypted metrics data based on specified conditions and authorization signature.
+ * @param {{ciphertext: string, dataToEncryptHash: string}} encryptMetrics - The encrypted metrics data.
+ * @param {string} questInvokerPKPAddress - The quest invoker's PKP address.
+ * @param {string} playerProfileOwnerAddress - The player profile owner's address.
+ * @param {LitAuthSig} playerPKPAuthSig - The player's PKP authorization signature.
+ * @param {LitJsSdk.LitNodeClient} litNodeClient - The Lit Node Client instance.
+ * @returns {Promise<{decryptedString?: string, error?: boolean, message?: string}>} -
+ *   A promise that resolves to an object containing either an error or the decrypted metrics string.
+ */
 export const decryptMetrics = async (
   encryptMetrics: { ciphertext: string; dataToEncryptHash: string },
   questInvokerPKPAddress: `0x${string}`,
@@ -319,6 +391,13 @@ export const decryptMetrics = async (
   }
 };
 
+/**
+ * @function mintNextPKP
+ * @description Asynchronously mints the next PKP (Personal Key Pair) token using a specified contract instance.
+ * @param {ethers.Contract} pkpContract - The contract instance for minting PKP tokens.
+ * @returns {Promise<{pkpTokenId?: string, publicKey?: `0x04${string}`, error?: boolean, message?: string}>} -
+ *    A promise that resolves to an object containing either a PKP token ID and public key or an error.
+ */
 export const mintNextPKP = async (
   pkpContract: ethers.Contract,
 ): Promise<{
@@ -342,6 +421,15 @@ export const mintNextPKP = async (
   }
 };
 
+/**
+ * @function assignLitAction
+ * @description Asynchronously assigns a Lit action to a specified token ID using a specified contract instance.
+ * @param {ethers.Contract} pkpPermissionsContract - The contract instance for managing PKP permissions.
+ * @param {string} tokenId - The token ID to which the Lit action is to be assigned.
+ * @param {string} bytesHash - The hash of the Lit action to be assigned.
+ * @returns {Promise<{txHash?: string, error?: boolean, message?: string}>} -
+ *    A promise that resolves to an object containing either a transaction hash or an error.
+ */
 export const assignLitAction = async (
   pkpPermissionsContract: ethers.Contract,
   tokenId: string,
@@ -362,6 +450,15 @@ export const assignLitAction = async (
   }
 };
 
+/**
+ * @function removeLitAction
+ * @description Asynchronously removes a Lit action from a specified token ID using a specified contract instance.
+ * @param {ethers.Contract} pkpPermissionsContract - The contract instance for managing PKP permissions.
+ * @param {string} tokenId - The token ID from which the Lit action is to be removed.
+ * @param {string} bytesHash - The hash of the Lit action to be removed.
+ * @returns {Promise<{txHash?: string, error?: boolean, message?: string}>} -
+ *    A promise that resolves to an object containing either a transaction hash or an error.
+ */
 export const removeLitAction = async (
   pkpPermissionsContract: ethers.Contract,
   tokenId: string,
@@ -381,6 +478,12 @@ export const removeLitAction = async (
   }
 };
 
+/**
+ * @function generateSecureRandomKey
+ * @description Generates a secure random key, only runnable in a Node.js environment due to dependency on Node's crypto module.
+ * @throws Will throw an error if function is run outside a Node.js environment.
+ * @returns {string} - The generated secure random key as a hexadecimal string.
+ */
 export const generateSecureRandomKey = () => {
   if (!crypto) {
     throw new Error("This function can only be run in a Node.js environment.");
@@ -388,6 +491,13 @@ export const generateSecureRandomKey = () => {
   return crypto.randomBytes(32).toString("hex");
 };
 
+/**
+ * @function getLitActionCode
+ * @description Generates a string of JavaScript code for a Lit action, given a conditional hash and contract address.
+ * @param {string} conditionalHash - The conditional hash to be used in the generated code.
+ * @param {string} contractAddress - The contract address to be used in the generated code.
+ * @returns {string} - The generated JavaScript code as a string.
+ */
 export const getLitActionCode = (
   conditionalHash: string,
   contractAddress: string,
@@ -437,6 +547,12 @@ export const getLitActionCode = (
   `;
 };
 
+/**
+ * @function bundleCodeManual
+ * @description Bundles dynamic code with a pre-defined bundled code string (DENO_BUNDLED).
+ * @param {string} dynamicCode - The dynamic code to be bundled.
+ * @returns {string} - The bundled code as a string.
+ */
 export const bundleCodeManual = (dynamicCode: string): string => {
   return DENO_BUNDLED + "\n\n" + dynamicCode;
 };
